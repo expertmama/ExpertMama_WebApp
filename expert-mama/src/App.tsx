fd5c0933744272d296e696b936c2f9ba0e7cecb9
Mohammad Rumman <expertmama.official@gmail.com>
2026-01-07 04:11:59 +0600

    Tailwind Istalation Check

--- expert-mama/src/App.tsx
+++ expert-mama/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
-
 function App() {
   return (
-    <div>
-      <h1>Expert Mama</h1>
-      <p>সমস্যা আপনার, সমাধান মামার।</p>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold text-orange-500">
+        Expert Mama
+      </h1>
+      <p className="mt-4 text-lg">
+        সমস্যা আপনার, সমাধান মামার।
+      </p>
     </div>
   );
 }
